ffc85697749e3e603fffd9d18f7298a23f428e31
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-01-18 11:59:23 +0530

    new project UI

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -13,9 +13,9 @@ export default function ProjectCard({
       className="outline outline-slate-400/35 rounded-lg mt-4 hover:shadow-lg/25 mx-3 md:mx-3"
       whileHover={{ scale: 1.01, transition: { duration: 0.4 } }}
     >
-      <div className="flex justify-between items-start border-b border-slate-400/30 px-4 py-3 mb-3.5">
+      <div className="flex justify-between items-start border-slate-400/30 px-4 pt-3 mb-3.5">
         <div>
-          <h2 className="text-base md:text-lg text-cyan-300 hover:text-blue-400">
+          <h2 className="text-base md:text-sm text-cyan-300 hover:text-blue-400">
             {liveLink ? (
               <a href={liveLink} target="_blank" rel="noopener noreferrer">
                 {name}
@@ -24,7 +24,7 @@ export default function ProjectCard({
               name
             )}
           </h2>
-          <p className="text-xs text-slate-300 mt-1">{type}</p>
+          <p className="text-[10px] text-slate-300 mt-1">{type}</p>
         </div>
         <div className="flex gap-3">
           {githubLink && (
@@ -32,7 +32,7 @@ export default function ProjectCard({
               href={githubLink}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-xs md:text-sm text-slate-400 hover:text-white underline"
+              className="text-xs md:text-xs text-slate-400 hover:text-white underline"
             >
               GitHub
             </a>
@@ -42,7 +42,7 @@ export default function ProjectCard({
               href={liveLink}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-xs md:text-sm text-slate-400 hover:text-white underline"
+              className="text-xs text-slate-400 hover:text-white underline"
             >
               Live
             </a>
@@ -51,20 +51,20 @@ export default function ProjectCard({
       </div>
 
       <div className="px-3">
-        <p className="text-xs md:text-sm text-slate-200 mb-2">{description}</p>
+        <p className="text-[12px] text-slate-200 mb-2">{description}</p>
 
-        {stack && stack.length > 0 && (
-          <div className="mt-4 text-sm space-x-2 text-slate-300 flex flex-wrap">
+        {/* {stack && stack.length > 0 && (
+          <div className="mt-4 text-xs space-x-2 text-slate-300 flex flex-wrap">
             {stack.map((tech: string, idx: number) => (
               <span
                 key={idx}
-                className="bg-slate-700/40 px-2 py-0.5 rounded text-xs mb-2"
+                className="bg-slate-700/40 px-2 py-0.5 rounded text-[10px] mb-2"
               >
                 {tech}
               </span>
             ))}
           </div>
-        )}
+        )} */}
       </div>
     </motion.div>
   );
--- src/components/tabContents/projects.tsx
+++ src/components/tabContents/projects.tsx
@@ -1,11 +1,12 @@
+import { div } from "motion/react-m";
 import { projects } from "../../contents/project.json"
 import ProjectCard from "../project-card.tsx"; 
 
 
 export default function Projects() {
     return (
-        <div className={`grid ${projects.length > 1 ? "md:grid-cols-3" : ""}`}
-
+            <div className={ `flex-col w-10/12 m-auto grid ${projects.length > 1 ? "md:grid-cols-2" : ""}`}
+          
         >
             {projects.map((item, idx) => (
                 <ProjectCard
